c85c17c32ca7dab649690b091a4e179245b47fc8
root <root@MiSTer-DEV.natetrix.com>
2018-11-05 19:42:45 -0500

    clean-up

--- serial.c
+++ serial.c
@@ -48,7 +48,6 @@ int serial_set_interface_attribs(int fd)
     }
 
     return 0;
-
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////
@@ -58,6 +57,5 @@ int serial_set_interface_attribs(int fd)
 
 void serial_do_tcdrain(int fd)
 {
-
     tcdrain(fd);
 }
