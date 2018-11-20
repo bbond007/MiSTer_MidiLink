69113a31d546b7aebe1a9b59629f6b4ea2190f4e
root <root@MiSTer-DEV.natetrix.com>
2018-11-19 23:34:57 -0500

    Support for MUNT/FluidSynth/Network UDP

--- misc.c
+++ misc.c
@@ -3,6 +3,13 @@
 #include <string.h>
 #include <ctype.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h> // close function
+
+
+
 
 #define TRUE  1
 #define FALSE 0
@@ -57,5 +64,57 @@ int misc_check_device (char * deviceName)
         return FALSE;
     }
     printf("TRUE\n"); 
+    return TRUE;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//
+// BOOL misc_check_device (char * fileName) 
+//
+int misc_check_file(char * fileName)
+{
+    return misc_check_device(fileName);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//
+// bool get_server_and_port(char * server)
+//
+
+int misc_get_server_and_port(char * server, int * pport)
+{
+    char buffer[30];
+    int port = -1;
+    char * strPort, * endPtr;
+    char MIDISERVER[] = "/media/fat/MIDISERVER";
+    int fdMidiServer;  
+    fdMidiServer = open (MIDISERVER, O_RDONLY);
+    if (fdMidiServer == -1) 
+    {
+        printf("ERROR unable to open %s\n", MIDISERVER);
+        return -1;
+    }
+    int rdLen = read(fdMidiServer, buffer, sizeof(buffer));
+    if (rdLen < 7)
+    {
+
+        printf("%s read length error\n",MIDISERVER);
+        close(fdMidiServer);
+        return -1; 
+    }
+
+    char * c = strchr(buffer, ':');
+    if (c != NULL) 
+    {
+        *c = (char) 0x00;
+        c++;
+        strPort = c;
+        port = strtol(strPort, &endPtr, 10);
+    }
+    close(fdMidiServer);
+    //printf("MIDI Server Address --> %s\n", buffer);
+    //printf("MIDI Server Port --> %d\n", port);
+    strcpy(server, buffer);
+    *pport = port;
     return TRUE;
 }
--- misc.h
+++ misc.h
@@ -1,4 +1,6 @@
 int misc_check_args_option (int argc, char *argv[], char * option);
 void misc_str_to_upper(char *str);
+int misc_check_file (char * fileName);
 int misc_check_device (char * deviceName);
+int misc_get_server_and_port(char * server, int * pport);
 
