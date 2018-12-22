8498ca474ff360b1d63ddbf89c5967a219bfba14
bbond007 <binarybond007@gmail.com>
2018-12-21 22:34:04 -0500

    UDP Listener

--- config.h
+++ config.h
@@ -14,7 +14,7 @@
 #define MIDI_INPUT
 static char          * midiLinkINI      = "/media/fat/config/MidiLink.INI";
 static char          * serialDevice     = "/dev/ttyS1";
-static char          * helloStr         = "BinaryBond007 MidiLink 1.4\n";
+static char          * helloStr         = "BinaryBond007 MidiLink 1.5\n";
 static unsigned char   test_note[3]     = {0x90, 60, 127};
 static char          * midi1Device      = "/dev/midi1";
 static char          * midiDevice       = "/dev/midi";
