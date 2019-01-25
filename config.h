1ac76b928b2fa1ea7890f582775d830da803d371
bbond007 <binarybond007@gmail.com>
2019-01-24 20:50:37 -0500

    change version #

--- config.h
+++ config.h
@@ -13,7 +13,7 @@
 
 static char          * midiLinkINI      = "/media/fat/linux/MidiLink.INI";
 static char          * serialDevice     = "/dev/ttyS1";
-static char          * helloStr         = "BinaryBond007 MidiLink 2.0";
+static char          * helloStr         = "MiSTer MidiLink 2.1 BB7";
 static unsigned char   test_note[3]     = {0x90, 60, 127};
 static char          * midiINDevice     = "/dev/midi2";
 static char          * midiDevice       = "/dev/midi1";
