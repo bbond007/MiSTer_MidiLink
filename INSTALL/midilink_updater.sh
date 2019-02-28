77d7a33ecb0c41418a3ba32aec4da1c8824463b4
bbond007 <binarybond007@gmail.com>
2019-02-28 01:34:08 -0500

    update outdated install info

--- INSTALL/midilink_updater.sh
+++ INSTALL/midilink_updater.sh
@@ -3,47 +3,51 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-MIDILINK=/media/fat/MidiLink.tar.gz
-KERNEL=/media/fat/linux/zImage_dtb
-KERNELOLD=/media/fat/linux/zImage_dtb.old
 SF_FILEID="1hO88em6bmTklHWo_WwEFnOgSFhlMEhws"
-SF_FILENAME="/media/fat/soundfonts/SC-55.sf2"
+SOUNDFONT_DIR="/media/fat/linux/soundfonts/"
+SF_FILENAME="$SOUNDFONT_DIR/SC-55.sf2"
 GD_COOKIE="/tmp/google_drive_cookie"
-MT32_ROM_DIR="/media/fat/mt32-rom-data/"
+MT32_ROM_DIR="/media/fat/linux/mt32-rom-data/"
 MT32_ROM_ZIP="$MT32_ROM_DIR/mt32_roms.zip"
+MP3_DIR="/media/fat/MP3"
+MIDI_DIR="/media/fat/MIDI"
+MIDILINK_BIN="/sbin/midilink"
 echo "Checking Internet..."
 if ! ping -q -w1 -c1 google.com &>/dev/null
 then
   echo "No Internet connection"
   exit 1
 fi
-echo "Installing MidiLink"
-uartmode 0
-sleep 1
-if [ -f $MIDILINK ]; 
-then
-  echo "Removing old MidiLink archive"
-  rm $MIDILINK
+if [ -d "$MT32_ROM_DIR" ]; then
+  echo "Found MT-32 ROM directory"
+else
+  echo "Creating MT-32 ROM directory"
+  mkdir $MT32_ROM_DIR
 fi
-echo "Downloading MidiLink archive"
-curl -kL https://github.com/bbond007/MiSTer_MidiLink/blob/master/INSTALL/MidiLink.tar.gz?raw=true -o $MIDILINK
-cd /
-echo "Remounting Linux Filesystem"
-/bin/mount -o remount,rw /
-echo "Extracting MidiLink Archive"
-tar -xvzf $MIDILINK
-rm $MIDILINK
-echo "Running depmod -a"
-depmod -a
+if [ -d "$SOUNDFONT_DIR" ]; then
+  echo "Found SoundFont directory"
+else
+  echo "Creating SoundFont directory"
+  mkdir $SOUNDFONT_DIR
+fi
+if [ -d "$MP3_DIR" ]; then
+  echo "Found MP3 directory"
+else
+  echo "Creating MP3 directory"
+  mkdir $MP3_DIR
+fi
+if [ -d "$MIDI_DIR" ]; then
+  echo "Found MIDI directory"
+else
+  echo "Creating MIDI directory"
+  mkdir $MIDI_DIR
+fi
 if [ -f $SF_FILENAME ];
 then 
   echo "SC-55 Soundfont found"
@@ -62,15 +66,18 @@ else
   echo "Unzipping CM-32 ROMS"
   unzip -o $MT32_ROM_ZIP CM32L* -d $MT32_ROM_DIR
 fi
-echo "Backing up Kernel"
-if [ -f $KERNELOLD ]; 
-then 
-  echo "Removing old backup Kernel"
-  rm $KERNELOLD
-fi 
-mv $KERNEL $KERNELOLD
-echo "Installing Kernel"
-curl -kL https://github.com/bbond007/MiSTer_MidiLink/blob/master/INSTALL/zImage_dtb?raw=true? -o $KERNEL
+echo "Remounting Linux Filesystem"
+/bin/mount -o remount,rw /
+echo "Setting uartmode 0"
+uartmode 0
+sleep 2
+if [ -f "$MIDILINK_BIN" ]; then
+  echo "Removing old MidiLink"
+else
+  echo "Old MidiLink not found"
+fi
+echo "Downloading MidiLink"
+curl -kL "https://github.com/bbond007/MiSTer_MidiLink/blob/master/midilink?raw=true" -o $MIDILINK_BIN
 echo "Rebooting in:"
 sleep 1
 echo "3"
@@ -80,6 +87,3 @@ sleep 1
 echo "1"
 sleep 1
 reboot
-
-
-
