12b40f494d77e5f168f0ccc9687a446e5d40e0db
BinaryBond007 <binarybond007@gmail.com>
2022-04-07 11:55:37 -0400

    UDPCSSW10 --> forgot to start UDP Synth loop :octocat:

--- main.c
+++ main.c
@@ -1014,6 +1014,7 @@ int main(int argc, char *argv[])
     case ModeUDPMUNT:
     case ModeUDPMUNTGM:
     case ModeUDPFSYNTH:
+	case ModeCSSW10:
         misc_print(0, "Starting --> UDP Synth loop :)\n");
         do
         {
