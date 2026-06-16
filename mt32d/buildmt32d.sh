01574644bb9c3ac03ddc555d2ded4a7901261706
root <root@Shovelhead.localdomain>
2026-06-15 23:36:41 -0400

    change ccompiler to gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf

--- mt32d/buildmt32d.sh
+++ mt32d/buildmt32d.sh
@@ -1,6 +1,6 @@
 #export PATH=/opt/gcc-linaro-6.5.0-2018.12-x86_64_arm-linux-gnueabihf/bin:$PATH
-export PATH=/opt/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf/bin:$PATH
-#export PATH=/opt/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin:$PATH
+#export PATH=/opt/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf/bin:$PATH
+export PATH=/opt/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin:$PATH
 #export PATH=/opt/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-linux-gnueabihf/bin/:$PATH
 rm -rf munt
 git clone --depth 1 https://github.com/munt/munt
